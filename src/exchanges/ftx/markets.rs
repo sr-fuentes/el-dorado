c41bd4937d3f30bfb0b421e0b8c981ad2fb100dd
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-08-01 11:19:39 -0700

    Added test to deserialize market struct

--- src/exchanges/ftx/markets.rs
+++ src/exchanges/ftx/markets.rs
@@ -1,4 +1,5 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+//use super::RestClient;
 
 // Sample 
 // '{"success":true,"result":[
@@ -26,7 +27,8 @@ use serde::Deserialize;
 // "volumeUsd24h":224063.1118
 // }
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Market {
     name: String,
     enabled: bool,
@@ -40,14 +42,58 @@ pub struct Market {
     price: f64,
     #[serde(rename="type")]
     market_type: String,
-    base_currency: String,
-    quote_currency: String,
-    underlying: String,
+    base_currency: Option<String>,
+    quote_currency: Option<String>,
+    underlying: Option<String>,
     restricted: bool,
-    high_leverage_fee_exemption: bool,
-    change_1h: f64,
-    change_24h: f64,
+    high_leverage_fee_exempt: bool,
+    change1h: f64,
+    change24h: f64,
     change_bod: f64,
-    quote_volume_24h: f64,
-    volume_usd_24h: f64,
+    quote_volume24h: f64,
+    volume_usd24h: f64,
+}
+
+pub type Markets = Vec<Market>;
+// impl RestClient {
+//     // Add `/market` specific API endpoints
+//     pub async fn get_markets(&self) -> Vec<Market> {
+//         self.get("/markets", None).await
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use crate::exchanges::ftx::*;
+
+    #[test]
+    fn serde_deserializes_the_market_struct () {
+        let market_json = r#"
+        {
+            "name":"AAVE/USD",
+            "enabled":true,
+            "postOnly":false,
+            "priceIncrement":0.01,
+            "sizeIncrement":0.01,
+            "minProvideSize":0.01,
+            "last":303.56,
+            "bid":304.98,
+            "ask":305.21,
+            "price":304.98,
+            "type":"spot",
+            "baseCurrency":"AAVE",
+            "quoteCurrency":"USD",
+            "underlying":null,
+            "restricted":false,
+            "highLeverageFeeExempt":true,
+            "change1h":-0.00029501425902251943,
+            "change24h":-0.011025358324145534,
+            "changeBod":-0.025186984593748,
+            "quoteVolume24h":224063.1118,
+            "volumeUsd24h":224063.1118
+        }
+        "#;
+        let deserialized: Market = serde_json::from_str(market_json).unwrap();
+        println!("deserialized: {:?}", deserialized);
+    }
 }
