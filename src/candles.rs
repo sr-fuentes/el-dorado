ecfd62b13d8d2bf849b93bd4301f02783e915af0
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-09-12 15:51:02 -0700

    Added impl candle_from_trades

--- src/candles.rs
+++ src/candles.rs
@@ -21,9 +21,43 @@ pub struct Candle {
 }
 
 impl Candle {
-    pub fn new_from_tuple() {}
-
-    pub fn new_from_trades(trades: Vec<Trade>) {}
+    pub fn new_from_trades(datetime: DateTime<Utc>, trades: Vec<Trade>) -> Self {
+        let candle_tuple = trades.iter().fold(
+            (
+                trades.first().expect("No trade to make candle.").price,
+                Decimal::MIN,
+                Decimal::MAX,
+                dec!(0),
+                dec!(0),
+                0,
+            ),
+            |(o, h, l, c, v, n), t| {
+                (
+                    o,
+                    h.max(t.price),
+                    l.min(t.price),
+                    t.price,
+                    v + t.size,
+                    n + 1,
+                )
+            },
+        );
+        Self {
+            datetime,
+            open: candle_tuple.0,
+            high: candle_tuple.1,
+            low: candle_tuple.2,
+            close: candle_tuple.3,
+            volume: candle_tuple.4,
+            volume_net: candle_tuple.4,         // todo!
+            volume_liquidation: candle_tuple.4, // todo!
+            value: candle_tuple.4,              // todo!
+            trade_count: 0,                     // todo!
+            liquidation_count: 0,               // todo!
+            last_trade_ts: datetime,            // todo!
+            last_trade_id: "TODO".to_string(),  // todo!
+        }
+    }
 
     pub fn new_from_last(
         datetime: DateTime<Utc>,
