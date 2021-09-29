2dd0cae03170aeb58f86d16ad630f5b32bb21b02
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-09-28 22:13:16 -0700

    Added error logging to rest api client

--- src/exchanges/ftx/client.rs
+++ src/exchanges/ftx/client.rs
@@ -88,6 +88,9 @@ impl RestClient {
                 if let Ok(ErrorResponse { error, .. }) = from_reader(&*response) {
                     Err(RestError::Api(error))
                 } else {
+                    println!("Reqwest resp: {:?}", std::str::from_utf8(&response).unwrap());
+                    println!("Error: {:?}", e.to_string());
+                    eprintln!("Errorpl: {:?}", e);
                     Err(e.into())
                 }
             }
