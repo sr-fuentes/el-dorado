f8121da85660ce35a14bb56de8fc8567aad8a12b
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-10-20 22:36:05 -0400

    Work on reading yaml config

--- scripts/compress_archive.py
+++ scripts/compress_archive.py
@@ -3,13 +3,20 @@
 
 import gzip
 import os
+import yaml
 # import pyarrow.csv as pv
 # import pyarrow.parquet as pq
 
+# Read config settings
+with open("../configuration/local.yaml") as file:
+    try:
+        config = yaml.safe_load(file)
+    except yaml.YAMLError as exc:
+        print(exc)
 path_exchanges = "../ed"
 exchanges = os.listdir(path_exchanges)
 for exchange in exchanges:
     path_csv = path_exchanges + "/" + exchange
     if os.isdir(path_csv):
-        csvs = os.listdir(pa)
+        csvs = os.listdir(path_csv)
     path_gzip = 
