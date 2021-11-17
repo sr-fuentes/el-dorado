9b43dc6ac146bb89f6eb40789af1a04378862e18
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-11-17 07:53:23 -0800

    Add candles and markets to validate

--- src/cleanup/ftx_candle_validations.rs
+++ src/cleanup/ftx_candle_validations.rs
@@ -1,6 +1,8 @@
+use crate::candles::Candle;
 use crate::configuration::Settings;
-use sqlx::PgPool;
 use crate::exchanges::{fetch_exchanges, ftx::RestClient};
+use crate::markets::fetch_markets;
+use sqlx::PgPool;
 
 // Clean up FTX candles that are not validated. Initial setup used a limit of 100 trades for each
 // trade api call. There are numerous instances where there were more than 100 trades in the
@@ -9,22 +11,46 @@ use crate::exchanges::{fetch_exchanges, ftx::RestClient};
 
 pub async fn cleanup_01(pool: &PgPool, config: Settings) {
     // Get exchanges from database
-    let exchanges = fetch_exchanges(pool).await.expect("Could not fetch exchanges.");
+    let exchanges = fetch_exchanges(pool)
+        .await
+        .expect("Could not fetch exchanges.");
     // Match exchange to config
-    let exchange = exchanges.iter().find(|e| e.exchange_name == config.application.exchange).unwrap();
+    let exchange = exchanges
+        .iter()
+        .find(|e| e.exchange_name == config.application.exchange)
+        .unwrap();
     // Get REST client for exchange
     let client = match exchange.exchange_name.as_str() {
         "ftxus" => RestClient::new_us(),
         "ftx" => RestClient::new_intl(),
         _ => panic!("No client exists for {}", exchange.exchange_name),
     };
-
+    // Get all markets and ids for markets
+    let market_ids = fetch_markets(pool, &exchange)
+        .await
+        .expect("Could not fetch markets.");
     // Get all hb candles that are not validated
     let sql = format!(
         r#"
         SELECT * FROM candles_15t_{}
         WHERE not is_validated
         ORDER BY market_id, time
-        "#, exchange.exchange_name
+        "#,
+        exchange.exchange_name
     );
+    let candles = sqlx::query_as::<_, Candle>(&sql)
+        .fetch_all(pool)
+        .await
+        .expect("Could not fetch invalid candles.");
+    // For each invalid candle - try to revalidate
+    for candle in candles.iter() {
+        let market = market_ids
+            .iter()
+            .find(|m| m.market_id == candle.market_id)
+            .unwrap();
+        println!(
+            "Attempting to revalidate: {:?} - {:?}",
+            &market.market_name, &candle.datetime
+        );
+    }
 }
