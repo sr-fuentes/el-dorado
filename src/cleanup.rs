0ac5f803e31171323607bc94f5035739495dae1e
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-11-18 10:54:46 -0800

    Added ftx daily candle revalidation

--- src/cleanup.rs
+++ src/cleanup.rs
@@ -1,5 +1,7 @@
+pub mod ftx_01d_revalidation;
 pub mod ftx_candle_validations;
 pub mod trade_index_and_candle_validation;
 
+pub use ftx_01d_revalidation::*;
 pub use ftx_candle_validations::*;
 pub use trade_index_and_candle_validation::*;
