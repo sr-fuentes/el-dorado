e59045c66b88b1be7f8c71d652a83a23ff7e7028
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-11-21 13:21:23 -0800

    Saving progress on cleanup 04

--- src/cleanup/ftx_15t_revalidation.rs
+++ src/cleanup/ftx_15t_revalidation.rs
@@ -1,4 +1,5 @@
 use sqlx::PgPool;
+use crate::configuration::Settings;
 
 // Pull all 01d candles that are left after the previous cleanups. The 01d that are not validated
 // have all 15t validated candles but were validated using a 1bps tolerance instead of the an exact
@@ -6,3 +7,58 @@ use sqlx::PgPool;
 // associated 15t candles to not validated and the trade count to -1. It will also delete all trades
 // that are associated with the 15t candles. Finally it will call the
 // previous cleanup scripts to clean up the 15t candles and 01d candle validations.
+
+pub async fn cleanup_04(pool: &PgPool, config: Settings) {
+    // Get exchanges from database
+    let exchanges = fetch_exchanges(pool)
+        .await
+        .expect("Could not fetch exchanges.");
+    // Match exchange to config
+    let exchange = exchanges
+        .iter()
+        .find(|e| e.exchange_name == config.application.exchange)
+        .unwrap();
+    // Get REST client for exchange
+    let client = match exchange.exchange_name.as_str() {
+        "ftxus" => RestClient::new_us(),
+        "ftx" => RestClient::new_intl(),
+        _ => panic!("No client exists for {}", exchange.exchange_name),
+    };
+    // Get all markets and ids for markets
+    let market_ids = fetch_markets(pool, &exchange)
+        .await
+        .expect("Could not fetch markets.");
+    // Get all 01d candles that are not validated
+    let sql = format!(
+        r#"
+        SELECT * FROM candles_01d
+        WHERE not is_validated
+        ORDER BY market_id, datetime
+        "#
+    );
+    let candles = sqlx::query_as::<_, DailyCandle>(&sql)
+        .fetch_all(pool)
+        .await
+        .expect("Could not fetch invalid daily candles.");
+    // For each invalid candle - mark the hb candles for revalidation
+    for candle in candles.iter() {
+        let market = market_ids
+            .iter()
+            .find(|m| m.market_id == candle.market_id)
+            .unwrap();
+        // Get hb candles
+        let hb_candles = select_candles_by_daterange(
+            pool,
+            &exchange.exchange_name,
+            &market.market_id,
+            candle.datetime,
+            candle.datetime + Duration::days(1),
+        )
+        .await
+        .expect("Could not fetch hb candles.");
+        // For each hb candle 
+        // - delete trades from each table
+        // - set validation status = false and trade_count = -1
+    }
+    
+}
