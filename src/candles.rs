9bdae85990a24a65f1347dc687ef0e65cd188c49
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-02 13:05:41 -0800

    Added create candle table function

--- src/candles.rs
+++ src/candles.rs
@@ -220,6 +220,40 @@ impl Candle {
     }
 }
 
+pub async fn create_exchange_candle_table(
+    pool: &PgPool,
+    exchange_name: &str,
+) -> Result<(), sqlx::Error> {
+    // Create candles table for exchange
+    let sql = format!(
+        r#"
+        CREATE TABLE IF NOT EXISTS candles_15t_{} (
+            datetime timestamptz NOT NULL,
+            open NUMERIC NOT NULL,
+            high NUMERIC NOT NULL,
+            low NUMERIC NOT NULL,
+            close NUMERIC NOT NULL,
+            volume NUMERIC NOT NULL,
+            volume_net NUMERIC NOT NULL,
+            volume_liquidation NUMERIC NOT NULL,
+            value NUMERIC NOT NULL,
+            trade_count BIGINT NOT NULL,
+            liquidation_count BIGINT NOT NULL,
+            last_trade_ts timestamptz NOT NULL,
+            last_trade_id TEXT NOT NULL,
+            is_validated BOOLEAN NOT NULL,
+            market_id uuid NOT NULL,
+            first_trade_ts timestamptz NOT NULL,
+            first_trade_id TEXT NOT NULL,
+            PRIMARY KEY (datetime, market_id)
+        )
+        "#,
+        exchange_name
+    );
+    sqlx::query(&sql).execute(pool).await?;
+    Ok(())
+}
+
 pub fn resample_candles(market_id: Uuid, candles: &Vec<Candle>, duration: Duration) -> Vec<Candle> {
     match candles.len() {
         0 => Vec::<Candle>::new(),
