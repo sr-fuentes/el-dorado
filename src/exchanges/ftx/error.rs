252c65b9a07c8503ec52f065c4cd74b61c9dad4b
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-06 07:58:55 -0800

    Added WSError enum

--- src/exchanges/ftx/error.rs
+++ src/exchanges/ftx/error.rs
@@ -1,4 +1,6 @@
 use thiserror::Error;
+use super::Channel;
+use tokio_tungstenite::tungstenite;
 
 #[derive(Debug, Error)]
 pub enum RestError {
@@ -8,4 +10,18 @@ pub enum RestError {
     Reqwest(#[from] reqwest::Error),
     #[error(transparent)]
     Json(#[from] serde_json::Error),
+}
+
+#[derive(Debug, Error)]
+pub enum WSError {
+    #[error("Not subscribed to this channel {0:?}")]
+    NotSubscribedToThisChannel(Channel),
+    #[error("Missing subscription confirmation")]
+    MissingSubscriptionConfirmation,
+    #[error("Socket is not authenticated")]
+    SocketNotAuthenticated,
+    #[error(transparent)]
+    Tungstenite(#[from] tungstenite::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
 }
