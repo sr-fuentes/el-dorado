0a24363945a8071c39d0d6c5dc049e63c04db696
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-21 15:08:45 -0800

    Added validation type enum

--- src/validation.rs
+++ src/validation.rs
@@ -1,7 +1,7 @@
 use sqlx::PgPool;
 use std::convert::TryFrom;
 
-#[derive(Debug, Clone, sqlx::Type)]
+#[derive(Debug, Clone)]
 pub struct CandleValidation {
     pub exchange_name: ExchangeName,
     pub market_name: String,
@@ -12,4 +12,31 @@ pub struct CandleValidation {
     pub processed_ts: Option<Datetime<Utc>>,
     pub validation_status: ValidationStatus,
     pub notes: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, sqlx::Type)]
+pub enum ValidationType {
+    Auto,
+    Manual,
+}
+
+impl ValidationType {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            ValidationType::Auto => "auto",
+            ValidationType::Manual => "manual",
+        }
+    }
+}
+
+impl TryFrom<String> for ValidationType {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "auto" => Ok(Self::Auto),
+            "manual" => Ok(Self::Manual),
+            other => Err(format!("{} is not a supported validation type.", other)),
+        }
+    }
 }
