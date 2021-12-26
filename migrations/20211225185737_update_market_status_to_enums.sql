f753e8fa43cae24bff3dee63c65373119ca402db
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-25 21:46:10 -0800

    Update statements for markets

--- migrations/20211225185737_update_market_status_to_enums.sql
+++ migrations/20211225185737_update_market_status_to_enums.sql
@@ -1,1 +1,14 @@
 -- Add migration script here
+-- update any existing market and market data statuses
+
+UPDATE markets
+SET market_status = 'active'
+WHERE market_status = 'Active';
+
+UPDATE markets
+SET market_data_status = 'new'
+WHERE market_data_status = 'New';
+
+UPDATE markets
+SET market_date_status = 'sync'
+WHERE market_data_status = 'Syncing';
