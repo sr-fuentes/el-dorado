6e447d66b9128b1be0ab0c5c3c0aa57f2a27fd0e
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-29 09:58:16 -0800

    Added metric struct and new impl

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,7 +1,10 @@
-use crate::candles::TimeFrame;
+use crate::candles::{Candle, TimeFrame};
+use chrono::{DateTime, Utc};
+use rust_decimal::prelude::*;
+use rust_decimal_macros::dec;
 
 impl TimeFrame {
-    pub fn lbps(&self) -> [i64] {
+    pub fn lbps(&self) -> [i64; 3] {
         match self {
             TimeFrame::T15 => [672, 2880, 8640],
             TimeFrame::H01 => [168, 720, 2160],
@@ -9,5 +12,46 @@ impl TimeFrame {
             TimeFrame::H12 => [14, 60, 180],
             TimeFrame::D01 => [7, 30, 90],
         }
+    }
+}
+
+pub struct Metric {
+    pub datetime: DateTime<Utc>,
+    pub time_frame: TimeFrame,
+    pub lbp: i64,
+    pub r#return: Decimal,
+    pub tr: Decimal,
+    pub n: Decimal,
+}
+
+impl Metric {
+    // Takes vec of candles for a time frame and calculates metrics for the period
+    pub fn new(_tf: TimeFrame, candles: &[Candle]) {
+        let metric = candles.iter().fold(
+            (
+                dec!(0),    // close
+                Vec::new(), // [return]
+                Vec::new(), // [true range]
+            ),
+            |(c, mut vr, mut vtr), can| {
+                let r = match c.is_zero() {
+                    true => c,
+                    false => can.close / c - dec!(1),
+                };
+                vr.push(r);
+                let tr = match c.is_zero() {
+                    true => c,
+                    false => {
+                        let hl = can.high - can.low;
+                        let hpdc = (can.high - c).abs();
+                        let pdcl = (c - can.low).abs();
+                        hl.max(hpdc).max(pdcl)
+                    }
+                };
+                vtr.push(tr);
+                (can.close, vr, vtr)
+            },
+        );
+        println!("Metric: {:?}", metric);
     }
 }
