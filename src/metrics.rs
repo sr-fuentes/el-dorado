0265f4f4fd08e2d12d5fe0d7367ee0571019e036
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2021-12-30 23:40:13 -0800

    Added z score function and complete metrics new

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,9 +1,8 @@
 use crate::candles::{Candle, TimeFrame};
 use crate::exchanges::ExchangeName;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 use rust_decimal::prelude::*;
 use rust_decimal_macros::dec;
-use std::collections::VecDeque;
 use uuid::Uuid;
 
 impl TimeFrame {
@@ -18,6 +17,7 @@ impl TimeFrame {
     }
 }
 
+#[derive(Debug)]
 pub struct Metric {
     pub market_id: Uuid,
     pub exchange_name: ExchangeName,
@@ -26,20 +26,31 @@ pub struct Metric {
     pub lbp: i64,
     pub close: Decimal,
     pub r: Decimal,
-    pub n: Decimal,
-    pub rs: Decimal,
-    pub trs: Decimal,
-    pub uws: Decimal,
-    pub mbs: Decimal,
-    pub lws: Decimal,
+    pub ema1: Decimal,
+    pub ema2: Decimal,
+    pub ema3: Decimal,
+    pub mv1: Decimal,
+    pub mv2: Decimal,
+    pub mv3: Decimal,
+    pub atr: Decimal,
+    pub vw: Decimal,
+    pub ma: Decimal,
+    pub ofz: Decimal,
+    pub vz: Decimal,
+    pub rz: Decimal,
+    pub trz: Decimal,
+    pub uwz: Decimal,
+    pub bz: Decimal,
+    pub lwz: Decimal,
 }
 
 impl Metric {
     // Takes vec of candles for a time frame and calculates metrics for the period
-    pub fn new(tf: TimeFrame, candles: &[Candle]) {
+    pub fn new(exchange: &ExchangeName, tf: TimeFrame, candles: &[Candle]) -> Vec<Metric> {
         // Get look back periods for TimeFrame
         let lbps = tf.lbps();
         let n = candles.len();
+        let datetime = candles[n - 1].datetime + Duration::seconds(tf.as_secs());
         // Iterate through candles and return Vecs of Decimals to use for calculations
         let vecs = candles.iter().fold(
             (
@@ -121,17 +132,17 @@ impl Metric {
             },
         );
         // Create empty vec to hold metrics
-        // let metrics = Vec::new();
+        let mut metrics = Vec::new();
         // Calculate the metrics
         let ema1 = Metric::ewma(&vecs.1, 7);
         let ema2 = Metric::ewma(&vecs.1, 30);
         let ema3 = Metric::ewma(&vecs.1, 90);
         let mv1: Decimal =
-            &vecs.5[n - 7..].iter().sum::<Decimal>() / &vecs.4[n - 7..].iter().sum::<Decimal>();
+            vecs.5[n - 7..].iter().sum::<Decimal>() / vecs.4[n - 7..].iter().sum::<Decimal>();
         let mv2: Decimal =
-            &vecs.5[n - 30..].iter().sum::<Decimal>() / &vecs.4[n - 30..].iter().sum::<Decimal>();
+            vecs.5[n - 30..].iter().sum::<Decimal>() / vecs.4[n - 30..].iter().sum::<Decimal>();
         let mv3: Decimal =
-            &vecs.5[n - 90..].iter().sum::<Decimal>() / &vecs.4[n - 90..].iter().sum::<Decimal>();
+            vecs.5[n - 90..].iter().sum::<Decimal>() / vecs.4[n - 90..].iter().sum::<Decimal>();
         // For each look back period, calc period specific metrics
         for lbp in &lbps[0..1] {
             // Set slice ranges
@@ -141,59 +152,44 @@ impl Metric {
             println!("Look Back Period: {}", lbp);
             // Calc metrics
             let atr = Metric::ewma(&vecs.8, *lbp);
-            let vw = &vecs.5[range_start..].iter().sum::<Decimal>()
-                / &vecs.4[range_start..].iter().sum::<Decimal>();
+            let vw = vecs.5[range_start..].iter().sum::<Decimal>()
+                / vecs.4[range_start..].iter().sum::<Decimal>();
             let ma = Metric::ewma(&vecs.1[..range_shift_end], *lbp);
-            let ofs = Metric::z(&vecs.6, range_shift_start, range_shift_end);
+            let ofz = Metric::z(&vecs.6, range_shift_start, range_shift_end);
+            let vz = Metric::z(&vecs.4, range_shift_start, range_shift_end);
+            let rz = Metric::z(&vecs.7, range_shift_start, range_shift_end);
+            let trz = Metric::z(&vecs.8, range_shift_start, range_shift_end);
+            let uwz = Metric::z(&vecs.9, range_shift_start, range_shift_end);
+            let bz = Metric::z(&vecs.10, range_shift_start, range_shift_end);
+            let lwz = Metric::z(&vecs.11, range_shift_start, range_shift_end);
+            let new_metric = Metric {
+                market_id: candles[0].market_id,
+                exchange_name: *exchange,
+                datetime,
+                time_frame: tf,
+                lbp: *lbp,
+                close: vecs.0,
+                r: vecs.7[n-1],
+                ema1,
+                ema2,
+                ema3,
+                mv1,
+                mv2,
+                mv3,
+                atr,
+                vw,
+                ma,
+                ofz,
+                vz,
+                rz,
+                trz,
+                uwz,
+                bz,
+                lwz,
+            };
+            metrics.push(new_metric);
         }
-
-        // pub market_id: Uuid,
-        // pub exchange_name: ExchangeName,
-        // pub datetime: DateTime<Utc>,
-        // pub time_frame: TimeFrame,
-        // pub lbp: i64,
-        // pub close: Decimal,
-        // pub r: Decimal,
-        // pub n: Decimal,
-        // pub rs: Decimal,
-        // pub trs: Decimal,
-        // pub uws: Decimal,
-        // pub mbs: Decimal,
-        // pub lws: Decimal,
-    }
-
-    pub fn new_deque(_tf: TimeFrame, candles: &[Candle]) {
-        let _lbp = 7;
-        let metric = candles.iter().fold(
-            (
-                dec!(0),         // close
-                VecDeque::new(), // [return]
-                dec!(0),         // running sum of return
-                VecDeque::new(),
-                dec!(0), // running sum of tr
-            ),
-            |(c, mut vr, sr, mut vtr, str), can| {
-                // Calc return as current candle close / previous candle close - 1. If first candle
-                // return 0. The initial zero should not be used when calculating mean.
-                let r = match c.is_zero() {
-                    true => c,
-                    false => can.close / c - dec!(1),
-                };
-                vr.push_back(r);
-                let tr = match c.is_zero() {
-                    true => c,
-                    false => {
-                        let hl = can.high - can.low;
-                        let hpdc = (can.high - c).abs();
-                        let pdcl = (c - can.low).abs();
-                        hl.max(hpdc).max(pdcl)
-                    }
-                };
-                vtr.push_back(tr);
-                (can.close, vr, sr, vtr, str)
-            },
-        );
-        println!("Metric: {:?}", metric);
+        metrics
     }
 
     pub fn ewma(v: &[Decimal], lbp: i64) -> Decimal {
@@ -201,8 +197,8 @@ impl Metric {
         let k = dec!(2) / (Decimal::from_i64(lbp).unwrap() + dec!(1));
         let ik = dec!(1.0) - k;
         let mut ewma = v[0];
-        for i in 1..v.len() {
-            ewma = v[i] * k + ewma * ik;
+        for i in v.iter().skip(1) {
+            ewma = i * k + ewma * ik;
         }
         ewma
     }
@@ -218,7 +214,7 @@ impl Metric {
             / n)
             .sqrt()
             .unwrap();
-        (v[v.len()] - shift_mean) / shift_sd
+        (v[v.len() - 1] - shift_mean) / shift_sd
     }
 }
 
@@ -243,7 +239,7 @@ mod tests {
             .await
             .expect("Failed to connect to Postgres.");
         // Get candles from db
-        let start = Utc.ymd(2021, 12, 25).and_hms(0, 0, 0);
+        let start = Utc.ymd(2021, 09, 25).and_hms(0, 0, 0);
         let candles = select_candles_gte_datetime(
             &pool,
             "ftxus",
@@ -254,10 +250,11 @@ mod tests {
         .expect("Failed to select candles.");
         println!("Num Candles: {:?}", candles.len());
         let timer_start = Utc::now();
-        let _metrics = Metric::new(TimeFrame::T15, &candles);
+        let metrics = Metric::new(&ExchangeName::Ftx, TimeFrame::T15, &candles);
         let timer_end = Utc::now();
         let time_elapsed = timer_end - timer_start;
         println!("Time to calc: {:?}", time_elapsed);
+        println!("Metrics: {:?}", metrics);
     }
 
     #[tokio::test]
@@ -327,5 +324,36 @@ mod tests {
         let ewma = Metric::ewma(&vtr.1, period);
         println!("Candle TRs: {:?}", vtr.1);
         println!("ATR {}: {:?}", period, ewma);
+    }
+
+    #[tokio::test]
+    pub async fn calc_z_return() {
+        // Get daily BTC-PERP candles from FTX
+        let client = crate::exchanges::ftx::RestClient::new_intl();
+        let mut candles = client
+            .get_candles("BTC-PERP", Some(86400), None, None)
+            .await
+            .expect("Failed to get candles.");
+        // Sort candles and put tr values into vector
+        candles.sort_by(|c1, c2| c1.time.cmp(&c2.time));
+        let vr = candles
+            .iter()
+            .fold((dec!(0), Vec::new()), |(c, mut vr), can| {
+                let r = match c.is_zero() {
+                    true => dec!(0),
+                    false => can.close / c - dec!(1),
+                };
+                vr.push(r);
+                (can.close, vr)
+            });
+        // Calc the z-score of return
+        let lbp = 90;
+        let n = vr.1.len();
+        let range_start = n - lbp as usize;
+        let range_shift_start = range_start - 1;
+        let range_shift_end = n - 1;
+        let z = Metric::z(&vr.1, range_shift_start, range_shift_end);
+        println!("vr: {:?}", vr.1);
+        println!("z: {:?}", z);
     }
 }
