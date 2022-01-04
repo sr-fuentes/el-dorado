b108bc67ca7dcee2fe7898f889b9d9cfdc585b14
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-01-03 17:42:42 -0800

    Added create events table migration

---
+++ migrations/20220103222230_create_events_table.sql
@@ -0,0 +1,27 @@
+-- Add migration script here
+
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events (
+    event_id UUID NOT NULL,
+    droplet TEXT NOT NULL,
+    exchange_name TEXT NOT NULL,
+    market_id TEXT NOT NULL,
+    start_ts timestamptz,
+    end_ts timestamptz,
+    event_ts timestamptz NOT NULL,
+    created_ts timestamptz NOT NULL,
+    processed_ts timestamptz,
+    event_status TEXT NOT NULL,
+    notes TEXT,
+    PRIMARY KEY (event_id)
+);
+
+CREATE INDEX IF NOT EXISTS events_droplet
+ON events (droplet);
+
+CREATE INDEX IF NOT EXISTS events_status
+ON events (event_status);
+
+CREATE INDEX IF NOT EXISTS events_ts
+ON events (event_ts);
