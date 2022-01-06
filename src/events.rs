0781bd2ff57eef3064b9a6104f034dfc60aed2f5
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-01-05 22:16:42 -0800

    Fixed defect in calculating time for next interval

--- src/events.rs
+++ src/events.rs
@@ -199,7 +199,7 @@ impl Inquisidor {
         }
         // Create event for next day
         let next_event_ts =
-            Utc::now().duration_trunc(TimeFrame::D01.as_dur()).unwrap() + Duration::minutes(2);
+            Utc::now().duration_trunc(TimeFrame::D01.as_dur()).unwrap() + Duration::days(1) + Duration::minutes(2);
         insert_event_create_daily_candles(&self.pool, &event.exchange_name, next_event_ts)
             .await
             .expect("Failed insert create daily candle event.");
