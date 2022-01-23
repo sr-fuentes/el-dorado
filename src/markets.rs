aaa74d5577aa8ec08a8155bd9ba53531c7c1d96a
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-01-22 23:10:46 -0500

    Added market rank logic and placeholders

--- src/markets.rs
+++ src/markets.rs
@@ -10,6 +10,7 @@ use sqlx::PgPool;
 use std::convert::{TryFrom, TryInto};
 use uuid::Uuid;
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 
 #[derive(Debug, PartialEq, Eq, sqlx::FromRow)]
 pub struct MarketId {
@@ -208,9 +209,36 @@ impl Inquisidor {
         filtered_markets.sort_by(|m1, m2| m2.volume_usd24h.cmp(&m1.volume_usd24h));
         // Create ranks table and select current contents
         create_market_ranks_table(&self.pool, &exchange).await.expect("Failed to create market ranks table.");
-        let current_ranks = select_market_ranks(&self.pool, &exchange).await.expect("Failed to select market ranks.");
+        let previous_ranks = select_market_ranks(&self.pool, &exchange).await.expect("Failed to select market ranks.");
+        // Create empty vec to hold new ranks
+        let mut new_ranks = Vec::new();
+        // Set rank counter = 1
+        let mut rank: i64 = 1;
         for market in filtered_markets.iter() {
-            println!("{}: {:?}", market.name, market.volume_usd24h);
+            // Check if there is a previous record for market
+            let previous_rank = previous_ranks.iter().find(|pr| pr.market_name == market.name);
+            let (rank_prev, mita_current) = match previous_rank {
+                Some(pr) => (Some(pr.rank), pr.mita_proposed.clone()),
+                None => (None, None),
+            };
+            let new_rank = MarketRank {
+                market_id: Uuid::new_v4(),
+                market_name: market.name.clone(),
+                rank,
+                rank_prev,
+                mita_current,
+                mita_proposed: None,
+                usd_volume_24h: market.volume_usd24h,
+                usd_volume_15t: market.volume_usd24h / dec!(96),
+                ats_v1: dec!(0),
+                ats_v2: dec!(0),
+                mps: dec!(1),
+                dp_quantity: 1,
+                dp_price: 1,
+                min_quantity: dec!(0),
+            };
+            new_ranks.push(new_rank);
+            rank += 1;
         }
     }
 }
