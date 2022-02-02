3df206c72e2efb7b318089ec2fa014f2daecc5f1
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-02-01 22:09:34 -0500

    Added request fn

--- src/exchanges/gdax/client.rs
+++ src/exchanges/gdax/client.rs
@@ -1,7 +1,7 @@
 use super::{RestError};
 use reqwest::{Client, Method};
 use serde::de::DeserializeOwned;
-use serde_json::Value;
+use serde_json::{from_reader, Map, Value};
 
 #[derive(Debug)]
 pub struct RestClient {
@@ -29,5 +29,51 @@ impl RestClient {
         params: Option<Value>,
     ) -> Result<T, RestError> {
         self.request(Method::GET, path, params).await
+    }
+
+    pub async fn request<T: DeserializeOwned>(
+        &self,
+        method: Method,
+        path: &str,
+        params: Option<Value>,
+    ) -> Result<T, RestError> {
+        let params = params.map(|value| {
+            if let Value::Object(map) = value {
+                map.into_iter()
+                    .filter(|(_, v)| v != &Value::Null)
+                    .collect::<Map<String, Value>>()
+            } else {
+                panic!("Invalid params.");
+            }
+        });
+
+        let response = self
+            .client
+            .request(method, format!("{}{}", self.endpoint, path))
+            .query(&params)
+            .send()
+            .await?;
+
+        match response.error_for_status() {
+            Ok(res) => {
+                let res_bytes = res.bytes().await?;
+                match from_reader(&*res_bytes) {
+                    Ok(result) => Ok(result),
+                    Err(e) => {
+                        println!(
+                            "Reqwest resp: {:?}",
+                            std::str::from_utf8(&res_bytes).unwrap()
+                        );
+                        println!("Error: {:?}", e.to_string());
+                        eprintln!("Errorpl: {:?}", e);
+                        Err(e.into())
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Reqwest status error: {:?}", e.status());
+                Err(e.into())
+            }
+        }
     }
 }
