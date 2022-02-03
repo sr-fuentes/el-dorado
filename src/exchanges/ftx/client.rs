77889bb6c41d3eb2f464184cc9ae90d5d24dc2d3
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-02-03 14:57:09 -0500

    Fixed unit tests for new client creation impls

--- src/exchanges/ftx/client.rs
+++ src/exchanges/ftx/client.rs
@@ -1,12 +1,11 @@
-use super::{RestError, Trade, WsError};
+use super::{Trade, WsError};
 use futures::{
     ready,
     task::{Context, Poll},
     Future, SinkExt, Stream, StreamExt,
 };
-use reqwest::{Client, Method};
-use serde::{de::DeserializeOwned, Deserialize};
-use serde_json::{from_reader, json, Map, Value};
+use serde::{Deserialize};
+use serde_json::json;
 use std::collections::VecDeque;
 use std::pin::Pin;
 use tokio::net::TcpStream;
@@ -14,30 +13,12 @@ use tokio::time;
 use tokio::time::{Duration, Interval};
 use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 
-#[derive(Debug)]
-pub struct RestClient {
-    pub header: &'static str,
-    pub endpoint: &'static str,
-    pub client: Client,
-}
 
 pub struct WsClient {
     channels: Vec<Channel>,
     stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
     buf: VecDeque<(Option<String>, Data)>,
     ping_timer: Interval,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct SuccessResponse<T> {
-    pub success: bool,
-    pub result: T,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct ErrorResponse {
-    pub success: bool,
-    pub error: String,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
@@ -81,95 +62,6 @@ pub enum ResponseData {
 #[derive(Clone, Debug)]
 pub enum Data {
     Trade(Trade),
-}
-
-impl RestClient {
-    pub const INTL_ENDPOINT: &'static str = "https://ftx.com/api";
-    pub const US_ENDPOINT: &'static str = "https://ftx.us/api";
-    pub const INTL_HEADER: &'static str = "FTX";
-    pub const US_HEADER: &'static str = "FTXUS";
-
-    pub fn new(endpoint: &'static str, header: &'static str) -> Self {
-        let client = Client::builder()
-            .timeout(std::time::Duration::from_secs(10))
-            .build()
-            .unwrap();
-        Self {
-            header,
-            endpoint,
-            client,
-        }
-    }
-
-    pub fn new_intl() -> Self {
-        Self::new(Self::INTL_ENDPOINT, Self::INTL_HEADER)
-    }
-
-    pub fn new_us() -> Self {
-        Self::new(Self::US_ENDPOINT, Self::US_HEADER)
-    }
-
-    pub async fn get<T: DeserializeOwned>(
-        &self,
-        path: &str,
-        params: Option<Value>,
-    ) -> Result<T, RestError> {
-        self.request(Method::GET, path, params).await
-    }
-
-    // pub fn post(&self, &url, &params) {}
-
-    pub async fn request<T: DeserializeOwned>(
-        &self,
-        method: Method,
-        path: &str,
-        params: Option<Value>,
-    ) -> Result<T, RestError> {
-        let params = params.map(|value| {
-            if let Value::Object(map) = value {
-                map.into_iter()
-                    .filter(|(_, v)| v != &Value::Null)
-                    .collect::<Map<String, Value>>()
-            } else {
-                panic!("Invalid params.");
-            }
-        });
-
-        let response = self
-            .client
-            .request(method, format!("{}{}", self.endpoint, path))
-            .query(&params)
-            .send()
-            .await?;
-        // .bytes()
-        // .await?;
-
-        match response.error_for_status() {
-            Ok(res) => {
-                let res_bytes = res.bytes().await?;
-                match from_reader(&*res_bytes) {
-                    Ok(SuccessResponse { result, .. }) => Ok(result),
-                    Err(e) => {
-                        if let Ok(ErrorResponse { error, .. }) = from_reader(&*res_bytes) {
-                            Err(RestError::Api(error))
-                        } else {
-                            println!(
-                                "Reqwest resp: {:?}",
-                                std::str::from_utf8(&res_bytes).unwrap()
-                            );
-                            println!("Error: {:?}", e.to_string());
-                            eprintln!("Errorpl: {:?}", e);
-                            Err(e.into())
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                println!("Reqwest status error: {:?}", e.status());
-                Err(e.into())
-            }
-        }
-    }
 }
 
 impl WsClient {
@@ -302,18 +194,19 @@ impl Stream for WsClient {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::exchanges::ftx::RestClient;
+    use crate::exchanges::client::RestClient;
+    use crate::exchanges::ExchangeName;
 
     #[test]
     fn new_intl_fn_returns_client_with_intl_header_and_endpoint() {
-        let client = RestClient::new_intl();
+        let client = RestClient::new(&ExchangeName::Ftx);
         assert_eq!(client.header, "FTX");
         assert_eq!(client.endpoint, "https://ftx.com/api");
     }
 
     #[test]
     fn new_us_fn_returns_client_with_us_header_and_endpoint() {
-        let client = RestClient::new_us();
+        let client = RestClient::new(&ExchangeName::FtxUs);
         assert_eq!(client.header, "FTXUS");
         assert_eq!(client.endpoint, "https://ftx.us/api");
     }
