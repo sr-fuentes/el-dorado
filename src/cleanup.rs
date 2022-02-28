8093f4450794606e6cc8c11d0a29a4b498875d7b
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-02-28 13:41:33 -0500

    Refactored candle rebuilding to daily chunks

--- src/cleanup.rs
+++ src/cleanup.rs
@@ -1,4 +1,4 @@
-use crate::candles::{insert_candle, select_candles, Candle, TimeFrame};
+use crate::candles::{insert_candle, select_candles, select_previous_candle, Candle, TimeFrame};
 use crate::exchanges::{gdax::Trade, ExchangeName};
 use crate::inquisidor::Inquisidor;
 use crate::markets::{select_market_details_by_status_exchange, MarketStatus};
@@ -87,9 +87,10 @@ impl Inquisidor {
                     market,
                     "validated",
                     *d,
-                    *d + TimeFrame::D01.as_dur())
-                    .await
-                    .expect("Failed to select trades from validated_table.");
+                    *d + TimeFrame::D01.as_dur(),
+                )
+                .await
+                .expect("Failed to select trades from validated_table.");
                 println!(
                     "{} Validated trades loaded for {:?}. Inserting into procesesing.",
                     validated_trades.len(),
@@ -105,7 +106,6 @@ impl Inquisidor {
                 .await
                 .expect("Failed to insert trades into processed.");
             }
-
 
             // c) Delete all hb candles
             let sql = r#"
@@ -123,84 +123,94 @@ impl Inquisidor {
             // d) Select all trades from processed and create candles for date range
             println!("Sleeping for 5 seconds to let trade inserts and deletes complete.");
             tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-            println!("Loading all procssed trades.");
-            let table = format!("trades_gdax_{}_processed", market.as_strip());
-            let mut trades = select_gdax_trades_by_table(&self.pool, &table)
+            for d in dr.iter() {
+                let mut trades = select_gdax_trades_by_time(
+                    &self.pool,
+                    &ExchangeName::Gdax,
+                    market,
+                    "processed",
+                    *d,
+                    *d + TimeFrame::D01.as_dur(),
+                )
                 .await
                 .expect("Failed to select trades from processed table.");
-            println!("Loaded all processed trades.");
+                println!(
+                    "Loaded {} processed trades for {:?} to make candles.",
+                    trades.len(),
+                    d
+                );
 
-            // e) Create new hb candles from processed trades
-            trades.sort_by(|t1, t2| t1.trade_id.cmp(&t2.trade_id));
-            let first_trade = trades.first().unwrap();
-            let last_trade = trades.last().unwrap();
-            println!(
-                "Creating new hb candles for trades starting {:?} to {:?}",
-                first_trade.time, last_trade.time
-            );
-            let mut can_start = first_trade
-                .time
-                .duration_trunc(TimeFrame::T15.as_dur())
-                .unwrap()
-                + TimeFrame::T15.as_dur();
-            let can_end = last_trade
-                .time
-                .duration_trunc(TimeFrame::T15.as_dur())
-                .unwrap();
-            let mut date_range = Vec::new();
-            while can_start < can_end {
-                date_range.push(can_start);
-                can_start = can_start + TimeFrame::T15.as_dur();
-            }
-            println!(
-                "Creating candles for dr from {:?} to {:?}",
-                date_range.first().unwrap(),
-                date_range.last().unwrap()
-            );
-            let mut previous_candle: Option<Candle> = None;
-            let opt_candles = date_range.iter().fold(Vec::new(), |mut v, d| {
-                let mut filtered_trades: Vec<Trade> = trades
-                    .iter()
-                    .filter(|t| t.time.duration_trunc(TimeFrame::T15.as_dur()).unwrap() == *d)
-                    .cloned()
-                    .collect();
-                let new_candle = match filtered_trades.len() {
-                    0 => previous_candle.as_ref().map(|pc| {
-                        Candle::new_from_last(
-                            market.market_id,
-                            *d,
-                            pc.close,
-                            pc.last_trade_ts,
-                            &pc.last_trade_id.to_string(),
-                        )
-                    }),
-                    _ => {
-                        filtered_trades.sort_by_key(|t1| t1.trade_id);
-                        Some(Candle::new_from_trades(
-                            market.market_id,
-                            *d,
-                            &filtered_trades,
-                        ))
-                    }
-                };
-                previous_candle = new_candle.clone();
-                v.push(new_candle);
-                v
-            });
-            let candles = opt_candles.into_iter().flatten().collect::<Vec<Candle>>();
-            println!("Created {} candles to insert.", candles.len());
-            for candle in candles.into_iter() {
-                insert_candle(
+                // e) Create new hb candles from processed trades
+                trades.sort_by(|t1, t2| t1.trade_id.cmp(&t2.trade_id));
+                let mut can_start = *d;
+                let can_end = *d + TimeFrame::D01.as_dur();
+                let mut date_range = Vec::new();
+                while can_start < can_end {
+                    date_range.push(can_start);
+                    can_start = can_start + TimeFrame::T15.as_dur();
+                }
+                println!(
+                    "Creating candles for dr from {:?} to {:?}",
+                    date_range.first().unwrap(),
+                    date_range.last().unwrap()
+                );
+                let mut previous_candle = match select_previous_candle(
                     &self.pool,
                     &ExchangeName::Gdax,
                     &market.market_id,
-                    candle,
-                    false,
+                    *d,
+                    TimeFrame::T15,
                 )
                 .await
-                .expect("Could not insert new candle.");
+                {
+                    Ok(c) => Some(c),
+                    Err(sqlx::Error::RowNotFound) => None,
+                    Err(e) => panic!("Sqlx Error: {:?}", e),
+                };
+                let opt_candles = date_range.iter().fold(Vec::new(), |mut v, d| {
+                    let mut filtered_trades: Vec<Trade> = trades
+                        .iter()
+                        .filter(|t| t.time.duration_trunc(TimeFrame::T15.as_dur()).unwrap() == *d)
+                        .cloned()
+                        .collect();
+                    let new_candle = match filtered_trades.len() {
+                        0 => previous_candle.as_ref().map(|pc| {
+                            Candle::new_from_last(
+                                market.market_id,
+                                *d,
+                                pc.close,
+                                pc.last_trade_ts,
+                                &pc.last_trade_id.to_string(),
+                            )
+                        }),
+                        _ => {
+                            filtered_trades.sort_by_key(|t1| t1.trade_id);
+                            Some(Candle::new_from_trades(
+                                market.market_id,
+                                *d,
+                                &filtered_trades,
+                            ))
+                        }
+                    };
+                    previous_candle = new_candle.clone();
+                    v.push(new_candle);
+                    v
+                });
+                let candles = opt_candles.into_iter().flatten().collect::<Vec<Candle>>();
+                println!("Created {} candles to insert.", candles.len());
+                for candle in candles.into_iter() {
+                    insert_candle(
+                        &self.pool,
+                        &ExchangeName::Gdax,
+                        &market.market_id,
+                        candle,
+                        false,
+                    )
+                    .await
+                    .expect("Could not insert new candle.");
+                }
+                println!("Inserted candles to db.");
             }
-            println!("Inserted candles to db.");
         }
         println!("GDAX cleanup complete.");
     }
