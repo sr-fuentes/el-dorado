406f3f1c98d2a451c3adea078465b4bb2d755e1e
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2022-04-06 16:47:45 -0400

    Added instance type and status struct and enums

--- src/utilities.rs
+++ src/utilities.rs
@@ -3,7 +3,9 @@ use rust_decimal::prelude::*;
 use rust_decimal_macros::dec;
 use std::env;
 use std::io::{self, Write};
+use std::convert::{TryFrom, TryInto};
 use twilio::{Client, OutboundMessage};
+use crate::exchanges::ExchangeName;
 
 pub struct Twilio {
     pub client: Client,
@@ -58,6 +60,85 @@ impl Twilio {
         };
     }
 }
+
+pub struct Instance {
+    pub r#type: InstanceType,
+    pub name: String,
+    pub exchange: ExchangeName,
+    pub status: InstanceStatus,
+    pub restart: bool,
+    pub last_retart: DateTime<Utc>,
+    pub restart_count: i32,
+    pub n_markets: i32,
+    pub last_update: DateTime<Utc>,
+}
+
+
+#[derive(Debug, Clone, Copy, sqlx::Type)]
+#[sqlx(rename_all = "lowercase")]
+pub enum InstanceType {
+    Mita,
+    Ig,
+}
+
+impl InstanceType {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            InstanceType::Mita => "mita",
+            InstanceType::Ig => "ig",
+        }
+    }
+}
+
+impl TryFrom<String> for InstanceType {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "mita" => Ok(Self::Mita),
+            "ig" => Ok(Self::Ig),
+            other => Err(format!("{} is not a supported instance.", other)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, sqlx::Type)]
+#[sqlx(rename_all = "lowercase")]
+pub enum InstanceStatus {
+    Sync,
+    Active,
+    Restart,
+    Paused,
+    Terminated,
+}
+
+impl InstanceStatus {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            InstanceStatus::Sync => "sync",
+            InstanceStatus::Active => "active",
+            InstanceStatus::Restart => "restart",
+            InstanceStatus::Paused => "paused",
+            InstanceStatus::Terminated => "terminated",
+        }
+    }
+}
+
+impl TryFrom<String> for InstanceStatus {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "sync" => Ok(Self::Sync),
+            "active" => Ok(Self::Active),
+            "restart" => Ok(Self::Restart),
+            "paused" => Ok(Self::Paused),
+            "terminated" => Ok(Self::Terminated),
+            other => Err(format!("{} is not a supported instance status.", other)),
+        }
+    }
+}
+
 
 pub fn get_input<U: std::str::FromStr>(prompt: &str) -> U {
     loop {
