48a424292aa975b424eea817feccb83db9fafb24
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2023-02-15 17:17:48 -0500

    Added archive to inqui loop

--- src/inquisidor.rs
+++ src/inquisidor.rs
@@ -10,6 +10,8 @@ impl ElDorado {
         loop {
             // Fill all active markets
             self.fill(&None, true).await;
+            // Archive all markets
+            self.archive(&None).await;
             // Sleep for 1 hour
             tokio::time::sleep(tokio::time::Duration::from_secs(3600)).await;
         }
